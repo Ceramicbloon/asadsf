2727f23e81beada7d1e1fd8273407a907a4a02ee
Ceramicbloon <79235916+Ceramicbloon@users.noreply.github.com>
2022-04-08 22:13:37 -0400

    Add files via upload

---
+++ Screens/Loading.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Text } from 'react-native'
+import firebaseConfig from '../config'
+
+export default class Loading extends React.Component {
+    render() {
+        return (
+            <Text>Loading</Text>
+        )
+    }
+    checkIfLoggedIn = () => {
+        firebase.auth().onAuthStateChanged(
+            user => {
+                if (user) {
+                    this.props.navigation.navigate('Dashboard')
+                } else {
+                    this.props.navigation.navigate('Login')
+                }
+            }
+        )
+
+    }
+    componentDidMount() {
+        this.checkIfLoggedIn()
+    }
+}
+
